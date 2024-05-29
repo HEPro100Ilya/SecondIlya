8e0339216383959f1869af678c0b89b27d9cf8ac
Psixushka <168550343+HEPro100Ilya@users.noreply.github.com>
2024-05-29 21:02:59 +0300

    Help

--- front/my-document-app/src/components/ui/CreateDoc.js
+++ front/my-document-app/src/components/ui/CreateDoc.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import { API_CREATE_DOC } from '../../utils/constants';
+import { API_CREATE_DOC } from '../utils/constants';
 
 const CreateDoc = () => {
     const [docName, setDocName] = useState('');
